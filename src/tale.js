84dbd2487fea6627d9bf581e1bdb8c71a0a56b49
Ildar1981 <fahriyevi@yandex.ru>
2023-04-12 10:29:25 +0300

    Домашняя по функциям

---
+++ src/tale.js
@@ -0,0 +1,29 @@
+let lisa = 1
+let deadushka = 2;
+let rabbit = 3;
+
+  switch(2) 
+  {
+    case  lisa:
+      alert( "меня съели" );
+      break;
+
+    case 2 :
+      alert( "я от дедушки ушел" );
+      break;
+
+  case 3:
+  alert("неизвестная операция")
+
+  }
+
+  let voice = 'Дед Мороз! Дед Мороз! Дед Мороз!'
+let name = 'santa'
+function newYear(name){
+    if(name=='santa'){
+        console.log(${voice})
+    }else{
+        console.log('Снегурочка! Снегурочка! Снегурочка!')
+    }
+}
+newYear(name)
